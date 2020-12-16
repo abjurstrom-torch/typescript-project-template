067df30ab48c62739831061acd4b680b9dc388ee
Adam Bjurstrom <adam.bjurstrom@torchtechnologies.com>
2020-12-16 08:21:06 -0600

    Added interface to Example class. Added jest config file with some neat defaults to run tests & coverage. Updated gitignore to exclude coverage reports.

--- src/Example.ts
+++ src/Example.ts
@@ -1,6 +1,13 @@
 import _ from "lodash";
+import IExample from "./IExample";
 
-export default class Example {
+export default class Example implements IExample {
+  /**
+   * "Processes" an array of numbers, returning a new array with each number doubled.
+   *
+   * @param input The array of numbers to process.
+   * @returns everything doubled.
+   */
   public ProcessNumbers(input: number[]): number[] {
     return _.map(input, (value) => value * 2);
   }
---
+++ src/IExample.ts
@@ -0,0 +1,3 @@
+export default interface IExample {
+  ProcessNumbers(input: number[]): number[];
+}
