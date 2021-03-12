827fb4705250e7ef4a1c03e7602df79adfbc368d
Adam Bjurstrom <adam.bjurstrom@torchtechnologies.com>
2021-03-12 13:52:15 -0600

    Added private class variable with default value to example class as a test for an issue that I was running into. Seems to work okay on newer version of node.  Leaving it in place so if someone is running an older version they can catch the trouble.

--- src/Example.ts
+++ src/Example.ts
@@ -3,6 +3,12 @@ import IExample from "./IExample";
 
 export default class Example implements IExample {
   /**
+   * Getting an error when running the sanity test on this assignment?  Try updating your node version!
+   * Saw some issues with v10 which was the default on the WSL image I was using.  Seems to work on v15.
+   */
+  private testAssignedVar = "abc123";
+
+  /**
    * "Processes" an array of numbers, returning a new array with each number doubled.
    *
    * @param input The array of numbers to process.
